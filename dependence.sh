45b7752bc99bf01b6786961e6de561ddcb0c01af
jcleng <j.c.leng@foxmail.com>
2026-04-18 00:15:22 +0800

    Add: excel-mcp
    Signed-off-by: jcleng <j.c.leng@foxmail.com>

--- dependence.sh
+++ dependence.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 uv tool install mcp-server-fetch
 uv tool install mysql_mcp_server
+uv tool install mcp-server-git
+uv tool install mindmap-mcp-server
 
 npm install -g @wonderwhy-er/desktop-commander
 npm install -g @upstash/context7-mcp
 npm install -g @modelcontextprotocol/server-sequential-thinking
+npm install -g @modelcontextprotocol/server-filesystem
+npm install -g @negokaz/excel-mcp-server
